07f12fa6a892503f0c79d41eb88abc16a2d7179c
Allison Ricker <alliricker@gmail.com>
2020-10-17 17:34:09 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,8 +16,8 @@ class Patient
   end
 
   def appointments
-      Appointment.all.select do |appointment|
-       appointment.patient == self
+    Appointment.all.select do |appointment|
+      appointment.patient == self
     end
   end
 end
